dcda6dbf8a3b927184c10dcad5ff96f745e400a4
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-04-10 22:03:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -1,13 +1,6 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b;
-    cin >> a >> b;
-    // Write Code to Swap
+void swap(int &a, int &b) {
+    // code here
     a=a+b;
     b=a-b;
     a=a-b;
-    cout << a << " " << b << endl;
-    return 0;
 }
