e4d957c09849cc6bb83a682bf0fb688831d1a270
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-04-15 20:39:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string URLify(string &s) {
+        // code here
+        string ans="";
+        for(auto &i:s)
+        {
+            if(i==' ')
+            {
+                ans+="%20";
+            }
+            else
+            {
+                ans+=i;
+            }
+        }
+        return ans;
+    }
+};
