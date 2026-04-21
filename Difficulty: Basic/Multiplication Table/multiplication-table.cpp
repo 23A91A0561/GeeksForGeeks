3bbf5a17387a4a54c90c8e1217d6162dbd0cb96a
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-04-21 10:13:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiplication Table/multiplication-table.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> getTable(int n) {
+        //  Code here
+        vector<int>ans;
+        for(int i=1;i<=10;i++)
+        {
+            ans.push_back(i*n);
+        }
+        return ans;
+    }
+};
