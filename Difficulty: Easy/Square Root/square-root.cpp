a97a1ba190eb6ade6c9771a8c8d209e00073608c
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-06-02 14:23:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // code here
+        int l=1,h=n;
+        int ans=1;
+        while(l<=h)
+        {
+            int m=l+(h-l)/2;
+            if(m*m<=n)
+            {
+                ans=m;
+                l=m+1;
+            }
+            else
+            {
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+};
