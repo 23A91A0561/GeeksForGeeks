85cda105bcae653138aa0dd6625e7fa95e71eb00
Tadepalli Jashwanth Sagar <23A91A0561@aec.edu.in>
2026-06-02 14:38:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        // code here
+        int l=0,h=arr.size()-1;
+        int ans=0;
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            if(arr[m]==k)
+            {
+                return m;
+            }
+            else if(arr[m]<k)
+            {
+               
+                l=m+1;
+            }
+            else
+            {
+                h=m-1;
+            }
+        }
+        return l;
+    }
+};
